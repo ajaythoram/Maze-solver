9d5908f3ed73d3274c5e9f22b616c7cc42d34d85
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2022-11-19 13:00:38 +0530

    Add files via upload

---
+++ MiniprojetMAZEsolver.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.miniprojet.mazesolver;
+
+
+public class MiniprojetMAZEsolver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
